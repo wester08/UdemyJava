7878a9c355d80e99c2598d1c4c693661a274e955
wester08 <wester.feliz08@gmail.com>
2023-10-06 08:25:21 -0400

    Tercer commit

---
+++ src/main/java/clases/Persona.java
@@ -0,0 +1,15 @@
+package clases;
+
+public class Persona {
+
+    //Atributos de la clase
+    String nombre;
+    String apellido;
+
+    //Metodos
+    public void desplegarInformacion(){
+        System.out.println("Nombre" + nombre);
+        System.out.println("Apellido" + apellido);
+    }
+
+}
